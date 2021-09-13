8684d72217b432ffbd96f1dcfdde465be3049799
Alex Marcelo <lete_alex@hotmail.com>
2021-09-13 19:06:12 -0300

    Comentei as linhas 9, 12 e 18 como foi pedido

--- CalcCirculo/Calculadora.cs
+++ CalcCirculo/Calculadora.cs
@@ -5,14 +5,17 @@ using System.Text;
 namespace CalcCirculo
 {
     class Calculadora
-    {
+    {   
+        //Cria a variavel com o valor de Pi
         public static double Pi = 3.14;
-
+        
+        //Fórmula para calcular a Circunferencia do raio
         public static double Circunferencia(double r)
         {
             return 2.0 * Pi * r;
         }
 
+        //Fórmula para Calcular o Volume do Raio
         public static double Volume(double r)
         {
             return 4.0 / 3.0 * Pi * r * r * r;
